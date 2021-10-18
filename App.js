a6806199f26d8f155a42612de5f3056e78de94df
Djóni Frank Strømsten <dfs@formula.fo>
2021-10-18 21:29:32 +0100

    .

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar, SafeAreaView } from 'react-native';
+
+const statusBarHeight = StatusBar.currentHeight; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={{flex:1, marginTop: statusBarHeight }}>
+        <View style={{ backgroundColor: 'green', padding: 16 }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'blue', padding: 16 }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
